1efeb45b3e072ddd108e4f9a8702f5e2aba7b7e6
TheoSimoes <theo.simoes@hotmail.fr>
2021-09-28 12:05:57 +0200

    u

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+let userHistory;
 chrome.history.search({text: ''}, history => {
+  userHistory = history; 
   console.log("🚀 ~ file: background.js ~ line 4 ~ history", history);
+  
+  document.getElementById('typedUrl_div').innerHTML = userHistory[0].title; 
 });
