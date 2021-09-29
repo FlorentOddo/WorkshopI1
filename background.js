7a498e3162582ec8101784d7d558a9f268f6c6d1
FlorentOddo <44065546+FlorentOddo@users.noreply.github.com>
2021-09-29 08:47:21 +0200

    Add active tab, florent le best

--- background.js
+++ background.js
@@ -5,3 +5,9 @@ chrome.history.search({text: ''}, history => {
   
   document.getElementById('typedUrl_div').innerHTML = userHistory[0].title; 
 });
+
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo,tab) {
+  console.log(changeInfo.url);
+});
