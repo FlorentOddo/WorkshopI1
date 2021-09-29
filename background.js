9f6b7b69c93d44f1349fbb3fe6688622bf1dd47d
TheoSimoes <theo.simoes@hotmail.fr>
2021-09-29 11:01:02 +0200

    base commit

--- background.js
+++ background.js
@@ -1,13 +1,8 @@
 let userHistory;
 chrome.history.search({text: ''}, history => {
   userHistory = history;   
-  document.getElementById('typedUrl_div').innerHTML = userHistory[0].title; 
 });
 
 
 
-  // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=> {
-  //   if(changeInfo.url){
-  //     // document.getElementById('typedUrls_div').innerHTML = changeInfo.url; 
-  //   }
-  // });
+
