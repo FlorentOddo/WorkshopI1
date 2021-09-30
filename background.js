e585d65d4ac715b007aabc12e551c984a081f778
FlorentOddo <44065546+FlorentOddo@users.noreply.github.com>
2021-09-30 08:56:05 +0200

    add code to background

--- background.js
+++ background.js
@@ -2,3 +2,52 @@ let userHistory;
 chrome.history.search({text: ''}, history => {
   userHistory = history;   
 });
+
+
+
+function doThing(urlComplet){
+  let domain = (new URL(urlComplet));
+    domain = domain.hostname;
+    let arraySite = domain.split('.');
+    let nameSite = arraySite[arraySite.length - 2];
+    let url = 'https://haveibeenpwned.com/api/v3/breach/'+nameSite;
+    console.log(url);
+    
+
+    fetch(url)
+    .then(response => {
+      if(response.status !== 404){
+        response.json()
+        .then(data => {
+          chrome.storage.sync.set({'breached': nameSite + " is breached"});  
+          console.log("il y a une faille");
+          }
+        );
+
+      }else{
+        chrome.storage.sync.set({'breached': nameSite + " is not breached"}); 
+        console.log("pas de réponse");
+      }
+    })
+    .catch(error => alert("Erreur : " + error));
+}
+
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+  if (change.url) {
+      console.log("you are here: "+change.url);  
+      chrome.storage.sync.set({'test': "test 28"});   
+      doThing(change.url);       
+  }
+});
+
+chrome.tabs.onActivated.addListener( function(activeInfo){
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+      y = tab.url;
+      console.log("you are here: "+y);
+      chrome.storage.sync.set({'test': "test 30"});
+      doThing(y); 
+  });
+});
+
+
--- script.js
+++ script.js
@@ -21,34 +21,6 @@ if(checkHist){
   });
 }
 
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=> {
-  if(changeInfo.url){
-
-    let domain = (new URL(changeInfo.url));
-    domain = domain.hostname;
-    let arraySite = domain.split('.');
-    nameSite = arraySite[arraySite.length - 2];
-    document.getElementById('pwned').innerHTML = nameSite; 
-    let url = 'https://haveibeenpwned.com/api/v3/breach/'+nameSite;
-    infoSite = "WARNING : " + nameSite + " HAS BEEN BREACHED";
-    document.getElementById('safe').innerHTML = infoSite;
-    
-
-    fetch(url)
-    .then(response => {
-      if(response.status !== 404){
-        response.json()
-        .then(data => {
-          console.log(data)
-          }
-        );
-
-      }else{
-        console.log("pas de réponse");
-      }
-    })
-    .catch(error => alert("Erreur : " + error));
-  
-  }
+chrome.storage.sync.get(["breached"], function(result){
+  document.getElementById('pwned').innerHTML = result.breached;
 });
